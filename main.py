c086d1977c6ef048cf6ea9c2b3023367acfacb3a
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-09-24 10:49:09 -0300

    implementing the new signature and setting 2 container's env vars

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import copy
 
-def create_flow(service_spec, deployment_spec, flow_uuid, api_key):
-    modified_deployment_spec = copy.deepcopy(deployment_spec)
+
+def create_flow(service_specs: list, deployment_specs: list, flow_uuid, api_key):
+    modified_deployment_specs = []
+
+    for deployment_spec in deployment_specs:
+        modified_deployment_spec = copy.deepcopy(deployment_spec)
+        container = modified_deployment_spec['template']['spec']['containers'][0]
+
+        # Add environment variables
+        container['env'] = container.get('env', []) + [
+            {'name': 'JSDELIVRAPIKEY', 'value': 'dev'},
+            {'name': 'FOO', 'value': 'BAR'},
+        ]
+
+        modified_deployment_spec['template']['spec']['containers'][0] = container
+        modified_deployment_specs.append(modified_deployment_spec)
+
 
     return {
-        "deployment_spec": modified_deployment_spec,
+        "deployment_specs": modified_deployment_specs,
         "config_map": {
         }
     }
+
 
 def delete_flow(config_map, flow_uuid):
     return
