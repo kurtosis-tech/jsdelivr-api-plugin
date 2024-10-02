270fbcc42f9a5f7ad5dd77cc357d538e431283ee
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-10-02 12:10:49 -0300

    add multiple deployment specs support

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 import copy
 
 
-def create_flow(service_specs: list, deployment_specs: list, flow_uuid, api_key):
+def create_flow(service_specs, deployment_specs, flow_uuid):
+
     modified_deployment_specs = []
 
     for deployment_spec in deployment_specs:
         modified_deployment_spec = copy.deepcopy(deployment_spec)
-        container = modified_deployment_spec['template']['spec']['containers'][0]
+        modified_deployment_spec['template']['spec']['containers'][0]["image"] = "kurtosistech/redis-proxy-overlay:latest"
 
-        # Add environment variables
-        container['env'] = container.get('env', []) + [
-            {'name': 'JSDELIVRAPIKEY', 'value': 'dev'},
-            {'name': 'FOO', 'value': 'BAR'},
-        ]
-
-        modified_deployment_spec['template']['spec']['containers'][0] = container
         modified_deployment_specs.append(modified_deployment_spec)
-
 
     return {
         "deployment_specs": modified_deployment_specs,
-        "config_map": {
-        }
+        "config_map": {}
     }
 
-
 def delete_flow(config_map, flow_uuid):
-    return
+    pass
